15fc44c544b24a91cc998086d01dcdf594186c21
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-05-22 20:25:13 -0300

    Adicionando entidades ao domínio

---
+++ BookingService/Core/Domain/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime PacedAt { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        private Status Status { get; set; }
+        public Status CurrentStatus { get { return Status; } }
+    }
+}
---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenance { get; set; }
+
+        public bool IsAvailable()
+        {
+            return !(this.InMaintenance || this.HasGuest());
+        }
+
+        public bool HasGuest()
+        {
+            //TODO: Verificar se existem bookings abertos para este quarto
+            return true;
+        }
+    }
+}
